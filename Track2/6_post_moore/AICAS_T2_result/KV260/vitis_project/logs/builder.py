65cc0a2caaac583aed7c5f1431769cb5df790fc2
IEEE-AICAS <aicas_code@outlook.com>
2025-04-23 00:13:58 +0800

    Delete Track2 directory

--- Track2/6_post_moore/AICAS_T2_result/KV260/vitis_project/logs/builder.py
+++
@@ -1,8 +0,0 @@
-# 2025-03-24T20:04:29.687069700
-import vitis
-
-client = vitis.create_client()
-client.set_workspace(path="E:/Desktop/LLM/competition/FPGA_GC/KV260/vitis_project")
-
-vitis.dispose()
-
--- Track2/6_post_moore/AICAS_T2_result/KV260/vitis_project/qwen_acc/build/CMakeFiles/qwen_acc.elf.dir/main.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/qwen_acc.elf.dir/main.c.obj: \
- E:/Desktop/LLM/competition/FPGA_GC/KV260/vitis_project/qwen_acc/src/main.c
